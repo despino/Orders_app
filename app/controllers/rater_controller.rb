dc18247a898d05246d8ac851c593cf93843f4858
despino <despino13@yahoo.com>
2015-11-17 15:21:31 -0800

    Tried to add breadcrumbs_on_rails, then switched to bootstrap crumbs and styling

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -3,13 +3,9 @@ class RaterController < ApplicationController
   def create
     if user_signed_in?
 
-      p '***********************************'
-      p params
 
       obj = params[:klass].classify.constantize.find(params[:id])
 
-      p '***********************************'
-      p obj
 
       obj.rate(params[:score].to_f, current_user, params[:dimension])
 
