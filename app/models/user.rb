14c4151c8628a8e412d96e520b5e03903c15ca87
Samuel Jaimes <sam.jaimes@gmail.com>
2015-11-18 14:48:49 -0800

    added Omniauth Twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook],
-         :omniauth_providers => [:twitter]
+         :omniauthable, :omniauth_providers => [:facebook, :twitter]
   has_many :groups, dependent: :destroy
 
-
-
   ratyrate_rater
+
+
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      if user.email.blank? && auth.provider == "twitter"
+        user.email = "#{auth.extra.raw_info.screen_name}@twitter.com"
+      end
       user.password = Devise.friendly_token[0,20]
       # user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
@@ -26,6 +29,5 @@ class User < ActiveRecord::Base
       end
     end
   end
-
 
 end
